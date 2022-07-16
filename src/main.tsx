d1daea5cc8f5eb5fc2a7833526a3d802a2bd9483
b-s-x <b.s.x.mars@gmail.com>
2022-07-16 22:02:02 +0300

    @/main.tsx: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'
+import Router from '@/router';
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Router />
     </BrowserRouter>
   </React.StrictMode>
 )
