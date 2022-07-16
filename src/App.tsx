8eacc8328d68293842965a428fcd09e30dc4e12b
b-s-x <b.s.x.mars@gmail.com>
2022-07-16 22:24:04 +0300

    @/App.tsx: return App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Router from '@/router';
+import Header from '@/components/Header';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  )
+}
+
+export default App;
