e962f5400bf461d4cac208a72881412ceb3181cc
b-s-x <b.s.x.mars@gmail.com>
2022-07-17 16:05:55 +0300

    @/components/Header/index.tsx: change

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,18 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <div className={styles.header}>
-        <Navbar />
+      <div className={styles.header_container}>
+        <InteriorIcon
+          className={styles.icon_interior}
+        />
+        <span className={styles.header_title}>
+          Interior
+        </span>
+      </div>
+
+      <Navbar />
     </div>
   )
 }
 
 export default Header;
-
-{/* <InteriorIcon
-        className={styles.icon_interior}
-      /> */}
