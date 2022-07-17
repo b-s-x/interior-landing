e369598a8f1a542ba4f814d4a28bc941a3229f25
b-s-x <b.s.x.mars@gmail.com>
2022-07-17 21:42:40 +0300

    @/constants/navMap.ts: add nav map

---
+++ src/constants/navMap.ts
@@ -0,0 +1,24 @@
+import { paths } from '@/constants/routes';
+
+export default [
+  {
+    paths: paths.home,
+    title: 'Home'
+  },
+  {
+    paths: paths.about,
+    title: 'About Us'
+  },
+  {
+    paths: paths.services,
+    title: 'Services'
+  },
+  {
+    paths: paths.pages,
+    title: 'Pages'
+  },
+  {
+    paths: paths.contacts,
+    title: 'Contact Us'
+  },
+];
