61e0fe29c92cabc07f2b05145420fe9b41003d04
b-s-x <b.s.x.mars@gmail.com>
2022-07-21 23:13:57 +0300

    @/components/Header/index: fix path

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import InteriorIcon from '@/icons/Interior';
-import Navbar from './components/Navbar';
+import Navbar from '@/components/Navbar';
 import styles from './Header.module.scss';
 
 const Header = () => {
