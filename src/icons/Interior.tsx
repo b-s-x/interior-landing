d9bf6237e2f31fd643b7adc61fc68935bc1d52d0
b-s-x <b.s.x.mars@gmail.com>
2022-07-25 22:57:55 +0300

    @/icons/Interior.tsx: fix space

--- src/icons/Interior.tsx
+++ src/icons/Interior.tsx
@@ -3,7 +3,7 @@ import { IIcon } from '@/types';
 
 const IconInterior = (props: IIcon) => {
   return (
-    <SvgContainer { ...props }>
+    <SvgContainer {...props}>
       <path
         d="M0 34h13.46v-9.5a3.64 3.64 0 117.29 0V34H34V0A34 34 0 000 34z"
       />
