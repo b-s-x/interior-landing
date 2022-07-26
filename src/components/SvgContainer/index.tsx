2922eb4007cef7a42ec035364b2ae8f0f1b622cf
b-s-x <b.s.x.mars@gmail.com>
2022-07-26 23:13:25 +0300

    @/components/SvgContainer/index.tsx: change props

--- src/components/SvgContainer/index.tsx
+++ src/components/SvgContainer/index.tsx
@@ -8,6 +8,7 @@ const SvgContainer = ({
   style,
   viewBox,
   children,
+  params,
 }: IIcon ) => {
   return (
     <svg
@@ -18,6 +19,7 @@ const SvgContainer = ({
       height={`${height}px`}
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
+      {...params}
     >
       {children}
     </svg>
