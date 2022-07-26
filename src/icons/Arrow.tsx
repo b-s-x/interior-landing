ee43a1b797bbd668e322aadda75f2bda33e1e1f5
b-s-x <b.s.x.mars@gmail.com>
2022-07-26 23:10:59 +0300

    @/icons/Arrow.tsx: add icon

---
+++ src/icons/Arrow.tsx
@@ -0,0 +1,19 @@
+import SvgContainer from '@/components/SvgContainer';
+import { IIcon } from '@/types';
+
+const IconInterior = (props: IIcon) => {
+  return (
+    <SvgContainer {...props}>
+      <path
+        d="m2 20 24-1M17.2 2 28 19.8 17 38"
+        strokeWidth="2"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+        fill="none"
+        stroke={props.color}
+      />
+    </SvgContainer>
+  )
+}
+
+export default IconInterior;
