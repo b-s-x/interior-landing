470f6817c83ab517672d938a6bbd79662fb4c05c
b-s-x <b.s.x.mars@gmail.com>
2022-07-28 09:17:48 +0300

    @/pages/Home/index.tsx: add screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import ImgMain from '@/assets/img/main.svg';
+import Button from '@/components/Button';
 import styles from './Home.module.scss';
 
 export const Home = () => {
+  const handleClick = () => console.log('Get Startedbutton click');
 
   return (
     <div>
-      Home
+      <div className={styles.img_container}>
+        <img
+          src={ImgMain}
+          alt="ImgMain"
+          className={styles.img}
+        />
+
+        <div className={styles.content_container}>
+          <div className={styles.content_top}>
+            Let's Create Your Dream Interior
+          </div>
+          <div className={styles.content_bottom}>
+            The world needs innovators and problem solvers who turn challenges into greater opportunities.
+          </div>
+
+          <Button
+            className={styles.btn}
+            handleClick={handleClick}
+          >
+            Get Started
+          </Button>
+        </div>
+
+      </div>
     </div>
   )
 };
