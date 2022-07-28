9cd2f71301c5b4c5322019751f8c26f9fa0d1532
b-s-x <b.s.x.mars@gmail.com>
2022-07-28 21:49:10 +0300

    src/pages/Home/index.tsx: add another screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { ScreenMain } from "./components/ScreenMain";
+import { ViewMore } from "./components/ViewMore";
 import styles from './Home.module.scss';
 
 export const Home = () => {
-
   return (
     <div>
       <ScreenMain />
+      <ViewMore />
     </div>
   )
 };
