bb7a95336f1767a150a773c37a3e36576e1f95d3
b-s-x <b.s.x.mars@gmail.com>
2022-07-28 09:25:19 +0300

    @/pages/Home/components: add Screenmain

---
+++ src/pages/Home/components/ScreenMain/index.tsx
@@ -0,0 +1,33 @@
+import ImgMain from '@/assets/img/main.svg';
+import Button from '@/components/Button';
+import styles from './ScreenMain.module.scss';
+
+export const ScreenMain = () => {
+  const handleClick = () => console.log('Get Startedbutton click');
+
+  return (
+    <div className={styles.img_container}>
+      <img
+        src={ImgMain}
+        alt="ImgMain"
+        className={styles.img}
+      />
+
+      <div className={styles.content_container}>
+        <div className={styles.content_top}>
+          Let's Create Your Dream Interior
+        </div>
+        <div className={styles.content_bottom}>
+          The world needs innovators and problem solvers who turn challenges into greater opportunities.
+        </div>
+
+        <Button
+          className={styles.btn}
+          handleClick={handleClick}
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  )
+}
