c7def531931df9ce7b7bea65c5bacb99a110c69a
b-s-x <b.s.x.mars@gmail.com>
2022-07-28 21:46:37 +0300

    src/mocks.ts: add mocks

---
+++ src/mocks.ts
@@ -0,0 +1,19 @@
+export const mocks = {
+  viewMore: [
+    {
+      id: 1,
+      title: 'Interior Design',
+      content: 'Enhancing the interior to achieve a healthier environment for the people using right space.',
+    },
+    {
+      id: 2,
+      title: 'Furniture',
+      content: 'Movable articles that are used to make a room or building suitable for living or working.',
+    },
+    {
+      id: 3,
+      title: 'Flooring',
+      content: 'Thin object any finished material applied over a floor structure to provide a walking surface.',
+    },
+  ]
+}
