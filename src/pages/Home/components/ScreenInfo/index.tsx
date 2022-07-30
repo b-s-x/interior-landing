658166789ce6f869223169040430874dbcddb114
b-s-x <b.s.x.mars@gmail.com>
2022-07-30 15:04:19 +0300

    src/pages/Home/components/ScreenInfo: add screen

---
+++ src/pages/Home/components/ScreenInfo/index.tsx
@@ -0,0 +1,36 @@
+import ImgScreenInfo from '@/assets/img/ImageScreenInfo.jpg';
+import { PhoneCard } from '@/components/PhoneCard';
+import { Button } from '@/components/Button';
+import styles from './ScreenInfo.module.scss';
+
+const TITLE = 'We Tackle The Most Challenging Designs';
+const TEXT = 'The world needs innovators and problem solvers who turn challenges into greater opportunities. We have an insatiable curiosity about transformative trends challenging the status.';
+
+export const ScreenInfo = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.container_content}>
+        <div className={styles.content_title}>
+          {TITLE}
+        </div>
+        <div className={styles.content_text}>
+          {TEXT}
+        </div>
+        <div className={styles.content_call}>
+          <PhoneCard />
+        </div>
+        <div className={styles.content_btn}>
+          <Button>
+            Get Free Estimate
+          </Button>
+        </div>
+      </div>
+      <div className={styles.container_img}>
+        <img
+          src={ImgScreenInfo}
+          alt="ImgScreenInfo"
+        />
+      </div>
+    </div>
+  )
+}
