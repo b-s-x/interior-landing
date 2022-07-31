1f405a4a8a516a96c7b2ccbff85bcf48ed55b0ae
b-s-x <b.s.x.mars@gmail.com>
2022-07-31 14:38:02 +0300

    src/pages/Home/components/ScreenBrand: add screen

---
+++ src/pages/Home/components/ScreenBrand/index.tsx
@@ -0,0 +1,30 @@
+import styles from './ScreenBrand.module.scss';
+import BrandLogo from '@/assets/brand/BrandLogo.svg';
+import ClassicLogo from '@/assets/brand/ClassicLogo.svg';
+import ModernHomeLogo from '@/assets/brand/ModernHomeLogo.svg';
+import NatureHomeLogo from '@/assets/brand/NatureHomeLogo.svg';
+import StyleVintageInteriorLogo from '@/assets/brand/StyleVintageInteriorLogo.svg';
+
+const BRANDS = [
+  ModernHomeLogo,
+  StyleVintageInteriorLogo,
+  BrandLogo,
+  NatureHomeLogo,
+  ClassicLogo,
+];
+
+export const ScreenBrand = () => {
+  return (
+    <div className={styles.container}>
+      {BRANDS.map(brand => {
+        return (
+          <img
+            src={brand}
+            alt={brand}
+            key={brand}
+          />
+        )
+      })}
+    </div>
+  )
+}
