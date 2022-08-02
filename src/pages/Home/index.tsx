0b0d89b2ccd6472fcf715b2b6ac0587705225c89
b-s-x <b.s.x.mars@gmail.com>
2022-08-02 09:48:27 +0300

    src/pages/Home/index.tsx: add screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { ScreenViewMore } from "./components/ScreenViewMore";
 import { ScreenInfo } from "./components/ScreenInfo";
 import { ScreenPeopleAbout } from "./components/ScreenPeopleAbout";
 import { ScreenBrand } from "./components/ScreenBrand";
+import { ScreenRecentProject } from "./components/ScreenRecentProject";
 import styles from './Home.module.scss';
 
 export const Home = () => {
@@ -13,6 +14,7 @@ export const Home = () => {
       <ScreenInfo />
       <ScreenPeopleAbout />
       <ScreenBrand />
+      <ScreenRecentProject />
     </div>
   )
 };
