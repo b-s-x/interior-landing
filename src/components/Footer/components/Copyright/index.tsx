b1e1e18ccf4f1cc883747e030b337defd53a10c9
b-s-x <b.s.x.mars@gmail.com>
2022-08-06 12:39:35 +0300

    src/components/Footer/components/Copyright/index.tsx: fix text

--- src/components/Footer/components/Copyright/index.tsx
+++ src/components/Footer/components/Copyright/index.tsx
@@ -3,13 +3,9 @@ import styles from './Copyright.module.scss';
 export const Copyright = () => {
   return (
     <div className={styles.copyright}>
-      Copyright © Interno | Designed by
+      Copyright © Interier | Designed by
       <span className={styles.gold}>
         Victorflow
-      </span>
-      Templates - Powered by
-      <span className={styles.gold}>
-        Webflow
       </span>
     </div>
   )
