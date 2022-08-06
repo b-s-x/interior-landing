09140c898d4a4387e468b027eaeadb91814b8ff3
b-s-x <b.s.x.mars@gmail.com>
2022-08-06 12:13:39 +0300

    src/App.tsx: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Router from '@/router';
 import Header from '@/components/Header';
+import { Footer } from './components/Footer';
 
 export const App = () => {
   return (
     <>
       <Header />
       <Router />
+      <Footer />
     </>
   )
 }
