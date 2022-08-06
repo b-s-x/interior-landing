4afa81cf85bffc2249ed8700140d6a64ec25e39e
b-s-x <b.s.x.mars@gmail.com>
2022-08-06 12:12:35 +0300

    src/constants/footer.ts: add footer constants

---
+++ src/constants/footer.ts
@@ -0,0 +1,27 @@
+interface IFooterContact {
+  address: string
+  mail: string
+  phone: string
+}
+
+export const pages: string[] = [
+  'Style Guide',
+  'Protected',
+  'Licenses',
+  'Changelog',
+  '404',
+];
+
+export const explore: string[] = [
+  'Contact Us',
+  'Our Projects',
+  'Pricing Plan',
+  'Recent Blog',
+  'About Us',
+];
+
+export const contact: IFooterContact = {
+  address: '53, East Birchwood Ave. Brooklyn, New York 11201, USA.',
+  mail: 'contact@interier.com',
+  phone: '(123) 456 - 7890',
+};
